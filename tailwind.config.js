0491fb4fbfe712b36f128968b008dfb6962bc76e
OlexandrSai <38455192+OlexandrSai@users.noreply.github.com>
2021-10-20 15:28:44 +0300

    Revert "added"
    This reverts commit 71f8c14bb3c60f496eebd2d0a659c55e7263b81e.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,10 @@ module.exports = {
   purge: [`./index.html`, `./src/**/*.{vue,js,ts,jsx,tsx}`],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        pink: {
-          500: "#FC00FF",
-        },
-        blue: {
-          400: "#00DBDE",
-          500: "#56CCF2",
-          600: "#2F80ED",
-        }
-      }
-    },
+    extend: {},
   },
   variants: {
-    extend: {
-      backgroundColor: ['active'],
-      backgroundImage: ['hover', 'focus', 'active'],
-      scale: ['active'],
-    },
+    extend: {},
   },
   plugins: [],
 }
